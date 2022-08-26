e026023ded50ead08455035b3730fff46b464ff5
bz <bailey7698@163.com>
2022-08-26 17:13:25 +0800

    使用插件切换中英文，insert模式下能自动恢复原先的输入法状态

--- init.lua
+++ init.lua
@@ -43,15 +43,15 @@ local config = {
 
 
   -- 退出insert模式后自动切换到英文输入法
-  vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-      pattern = { "*" },
-      callback = function()
-          local input_status = tonumber(vim.fn.system("fcitx5-remote"))
-          if input_status == 2 then
-              vim.fn.system("fcitx5-remote -c")
-          end
-      end,
-  }),
+  --[[ vim.api.nvim_create_autocmd({ "InsertLeave" }, { ]]
+  --[[     pattern = { "*" }, ]]
+  --[[     callback = function() ]]
+  --[[         local input_status = tonumber(vim.fn.system("fcitx5-remote")) ]]
+  --[[         if input_status == 2 then ]]
+  --[[             vim.fn.system("fcitx5-remote -c") ]]
+  --[[         end ]]
+  --[[     end, ]]
+  --[[ }), ]]
 
 
   -- Default theme configuration
@@ -160,6 +160,9 @@ local config = {
           -- vim.opt.background = "dark"
 	      end,
       },
+
+      -- fcitx5 输入法自动切换
+      { "h-hg/fcitx.nvim" },
 
       -- status line
 
